8843aa5a14b6222905020032c7c9f8207ea1ce1e
av1998av <av1998av@gmail.com>
2021-12-15 00:18:49 +0530

    added login screen

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -15,7 +16,7 @@ class LoginPageState extends State<LoginPage> {
   @override
   void initState(){
     super.initState();
-    Timer(const Duration(seconds: 3), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => const SecondScreen())));
+    Timer(const Duration(seconds: 3), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => SecondScreen())));
   }
   
   @override
@@ -28,16 +29,165 @@ class LoginPageState extends State<LoginPage> {
 }
 
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key}) : super(key: key);
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
+  SecondScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-	    body: Center(
-	      child: Text(
-	        "Login Page",
-	        style: TextStyle(fontSize: 25.0),
-	      )),
-	    );
-	  }
+    return Scaffold(
+    backgroundColor: Colors.white,
+    body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 400,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.fill
+              )
+            ),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  left: 30,
+                  width: 80,
+                  height: 200,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/light-1.png')
+                      )
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 140,
+                  width: 80,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/light-2.png')
+                      )
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 40,
+                  top: 40,
+                  width: 80,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/clock.png')
+                      )
+                    ),
+                  )
+                ),
+                Positioned(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 50),
+                    child: Center(
+                      child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.indigo[50],
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color.fromRGBO(143, 148, 251, .2),
+                        blurRadius: 20.0,
+                        offset: Offset(0, 10)
+                      )
+                    ]
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: userNameController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(15.0),
+                          border: InputBorder.none,
+                          hintText: "Username",
+                          hintStyle: TextStyle(color: Colors.black)
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30,),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.indigo[50],
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color.fromRGBO(143, 148, 251, .2),
+                        blurRadius: 20.0,
+                        offset: Offset(0, 10)
+                      )
+                    ]
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(15.0),
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          hintStyle: TextStyle(color: Colors.black)
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30,),
+                InkWell(
+                  child : Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(
+                        colors: const [
+                          Color.fromRGBO(143, 148, 251, 1),
+                          Color.fromRGBO(143, 148, 251, 1),
+                        ]
+                      )
+                    ),
+                    child: Center(
+                      child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),),
+                    ),
+                  ),
+                  onTap: () async {
+                    String username = userNameController.text;
+                    String password = passwordController.text;
+                    if (username == 'testUser' && password == 'test1234') {
+                      Navigator.pushNamed(context, '/appointments');
+                    }
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    )
+  );
   }
+}
